5016c30541931a1053bfadec19a444776c2e2680
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-02-18 16:47:19 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -131,6 +131,4 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function () {
-    return view('hello', ['name' => 'Danendra']);
-});
+Route::get('/greeting', [WelcomeController::class,'greeting']);
